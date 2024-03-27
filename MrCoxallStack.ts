d98396368c4daba8d660b208c2ca20522ea94ee1
Mr Coxall <mr.coxall@mths.ca>
2024-03-27 14:08:34 +0000

    added code

---
+++ MrCoxallStack.ts
@@ -0,0 +1,38 @@
+/**
+ * This is the MrCoxallStack class
+ *
+ * By:      Feyi Akomolafe
+ * Version: 1.0
+ * Since:   2024-03-25
+ */ 
+
+export class MrCoxallStack {
+  // Stack that contains all of the string
+  private stackAsList
+	
+  // Creates the stack
+  public constructor() {
+    this.stackAsList = []
+  }
+	
+  // Pushes the values to the stack
+  public push(stringPushed) {
+    this.stackAsList.push(stringPushed)
+  }
+	
+  // Returns the stack
+  public showStack() {
+    let stackItems = ""
+	                                   
+    for (let counter = 0; counter < this.stackAsList.length; counter++) {
+      // If statments ensures that comma is not printed at the start
+      if (counter === 0) {
+	stackItems += this.stackAsList[counter]
+      } else {
+	stackItems += ", " + this.stackAsList[counter]
+      }
+    }
+	
+    return stackItems
+  }
+}
---
+++ index.ts
@@ -0,0 +1,37 @@
+/**
+ * This is the main program
+ *
+ * By:      Feyi Akomolafe
+ * Version: 1.0
+ * Since:   2024-03-25
+ */ 
+
+import { MrCoxallStack } from './MrCoxallStack'
+
+// Uses MrCoxallStack
+const fruitStack = new MrCoxallStack()
+fruitStack.push("apple")
+fruitStack.push("banana")
+fruitStack.push("cherry")
+
+const colorStack = new MrCoxallStack()
+colorStack.push("red")
+colorStack.push("green")
+colorStack.push("blue")
+
+const dogStack = new MrCoxallStack()
+dogStack.push("poodle")
+dogStack.push("retriever")
+dogStack.push("beagle")
+
+// Shows the fruit stack items
+console.log("Fruit items:", fruitStack.showStack())
+
+// Shows the color stack items
+console.log("Color items:", colorStack.showStack())
+
+// Shows the dog stack items
+console.log("Dog items: ", dogStack.showStack())
+
+// Show the program as done
+console.log('\nDone.')
